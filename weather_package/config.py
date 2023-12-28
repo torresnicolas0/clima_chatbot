4712fc0297869a12e1db56861be2a70f78a0bf19
Nicolás Torres <torresnicolas@gmail.com>
2023-12-28 00:21:17 +0000

    Update config.py
    Removal of outdated comments.

--- weather_package/config.py
+++ weather_package/config.py
@@ -6,7 +6,6 @@ from log_config import get_logger
 
 logger = get_logger(__name__)
 
-# Carga de variables de entorno
 load_dotenv()
 
 class Config:
@@ -24,23 +23,10 @@ class Config:
     - LANGUAGE (str): Idioma para las respuestas de la API (por defecto: 'es').
     """
 
-    # Clave API de OpenWeatherMap. Debe ser reemplazada por una clave API válida obtenida del usuario.
     OW_API_KEY = os.getenv('OW_API_KEY')
 
-    # URL base para las solicitudes a la API de OpenWeatherMap.
     OW_URL = "http://api.openweathermap.org/data/2.5/weather?"
 
-    # Unidades de medida para los datos del clima.
-    # Opciones disponibles: 'metric' (Celsius), 'imperial' (Fahrenheit), o 'standard' (Kelvin).
     UNITS = 'metric'
 
-    # Idioma para las respuestas de la API.
-    # El idioma se configura según el código de idioma (por ejemplo, 'es' para español).
     LANGUAGE = 'es'
-
-    """
-    Nota importante:
-    La clave API (OW_API_KEY) debe mantenerse segura y no debe ser compartida públicamente.
-    Es recomendable obtener esta clave desde una variable de entorno o un archivo de configuración
-    externo en un entorno de producción para mantener la seguridad.
-    """
